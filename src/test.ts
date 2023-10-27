55f4ebc3faa4c7bb88824cad35491421dfd721b2
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-27 15:26:14 +0200

    feat: support async tests

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,43 @@
 /**
  * Encapsulate assertion and provide a description of it
  */
-export const test = (description: string, callback: () => void) => {
+export const test = (
+  description: string,
+  assertionContext: () => void | Promise<void>
+) => {
+  try {
+    isPromise(assertionContext())
+      ? callAsync(
+          description,
+          assertionContext as unknown as () => Promise<void>
+        )
+      : call(description, assertionContext);
+  } catch (error) {
+    console.log(`❌ ${description}`);
+  }
+};
+
+export const it = test;
+
+/**
+ * check if parameter is a promise.
+ */
+const isPromise = (maybePromise: void | Promise<void>) =>
+  !!maybePromise &&
+  typeof (maybePromise as unknown as Promise<unknown>).then === "function";
+
+/**
+ * call sync method
+ */
+const call = (description: string, callback: () => void) => {
   callback();
   console.log(`✅ ${description}`);
 };
 
-export const it = test
+/**
+ * call async method
+ */
+const callAsync = (description: string, callback: () => Promise<void>) =>
+  callback()
+    .then(() => console.log(`✅ ${description}`))
+    .catch((error) => console.log(`❌ ${description}`));
