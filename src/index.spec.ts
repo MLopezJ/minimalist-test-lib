c569b42e1d2b1bdcaf0d6aeb9aab1ee86e4a8f01
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-27 14:11:47 +0200

    fix: use test method

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,18 @@
 import { expect } from "./assertion";
-import {sum} from './index.js'
+import { test, it } from "./test";
+import { sum, average } from "./index.js";
 
-const result_sum = sum([1,2,3])
-const expected_sum = 6
+test("should sum  methods in array", () => {
+  const result_sum = sum([1, 2, 3]);
+  const expected_sum = 6;
 
-expect(result_sum).toBe(expected_sum)
+  expect(result_sum).toBe(expected_sum);
+});
+
+
+it("should return average from methods in array", () => {
+    const result_sum = average([1, 2, 3]);
+    const expected_sum = 2;
+  
+    expect(result_sum).toBe(expected_sum);
+  });
