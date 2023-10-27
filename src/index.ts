ca30088455994b9d893e41004d498a4f36ff996f
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-27 15:26:48 +0200

    feat: add async basic methods

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,24 @@
 /**
  * Sum elements in array
  */
-export const sum = (numbers: number[]): number => numbers.reduce((prev, current) => prev + current, 0)
+export const sum = (numbers: number[]): number =>
+  numbers.reduce((prev, current) => prev + current, 0);
+/**
+ * Async sum
+ */
+export const asyncSum = (numbers: number[]): Promise<number> =>
+  Promise.resolve(sum(numbers));
 
 /**
  * Get average of elements in array
  */
-export const average = (numbers: number[]) : number => sum(numbers) / numbers.length
+export const average = (numbers: number[]): number =>
+  sum(numbers) / numbers.length;
 
+/**
+ * Async average
+ */
+export const asyncAverage = (numbers: number[]): Promise<number> =>
+  Promise.resolve(average(numbers));
 
-
-// npx tsx src/index.ts 
+// npx tsx src/index.ts
