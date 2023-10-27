0de253ffcf3dea366045c1e312a32af9670dccfd
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-27 13:58:33 +0200

    fix: export basic functions

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,13 @@
 /**
  * Sum elements in array
  */
-const sum = (numbers: number[]): number => numbers.reduce((prev, current) => prev + current, 0)
+export const sum = (numbers: number[]): number => numbers.reduce((prev, current) => prev + current, 0)
 
 /**
  * Get average of elements in array
  */
-const average = (numbers: number[]) : number => sum(numbers) / numbers.length
+export const average = (numbers: number[]) : number => sum(numbers) / numbers.length
 
 
-console.log(sum([1,2,3]))
-console.log(average([1,2,3]))
+
 // npx tsx src/index.ts 
