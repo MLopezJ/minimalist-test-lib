3c1d8cc7b9f6228611c3b5753e47896abae376f8
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-27 14:17:25 +0200

    fix: use 'describe' method

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,18 +1,23 @@
 import { expect } from "./assertion";
 import { test, it } from "./test";
+import { describe } from './describe'
 import { sum, average } from "./index.js";
 
-test("should sum  methods in array", () => {
-  const result_sum = sum([1, 2, 3]);
-  const expected_sum = 6;
+describe('sum()', () => {
+  test("should sum  methods in array", () => {
+    const result_sum = sum([1, 2, 3]);
+    const expected_sum = 6;
+  
+    expect(result_sum).toBe(expected_sum);
+  });
+})
 
-  expect(result_sum).toBe(expected_sum);
-});
 
-
-it("should return average from methods in array", () => {
+describe('average', () => {
+  it("should return average from methods in array", () => {
     const result_sum = average([1, 2, 3]);
     const expected_sum = 2;
   
     expect(result_sum).toBe(expected_sum);
   });
+})
