1ddb33d97445b71c39b7b1264e544fec963e3ccf
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-10-27 15:27:15 +0200

    test: check async methods

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,18 @@
 import { expect } from "./assertion";
 import { test, it } from "./test";
 import { describe } from './describe'
-import { sum, average } from "./index.js";
+import { sum, average, asyncSum } from "./index.js";
 
 describe('sum()', () => {
   test("should sum  methods in array", () => {
     const result_sum = sum([1, 2, 3]);
+    const expected_sum = 6;
+  
+    expect(result_sum).toBe(expected_sum);
+  });
+
+  test("should sum  methods in array (async)", async() => {
+    const result_sum = await asyncSum([1, 2, 3]);
     const expected_sum = 6;
   
     expect(result_sum).toBe(expected_sum);
